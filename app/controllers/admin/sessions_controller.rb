1cc66ea696678813ac7b2613b0e0b502aacd2b8d
Hein Dauven <heindauven@gmail.com>
2016-09-22 10:40:41 +0200

    Implemented basic authentication.

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,5 +5,18 @@ class Admin::SessionsController < Admin::ApplicationController
   end
 
   def create
+    user = User.authentication(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to admin_posts_path, :notice => "Logged In"
+    else
+      flash.now.alert = "Invalid email or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to admin_login_path, :notice => 'Logged Out'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    get 'logout' => 'sessions#destroy', :as => 'logout'
+  end
+
+  namespace :admin do
     get 'sessions/create'
   end
 
